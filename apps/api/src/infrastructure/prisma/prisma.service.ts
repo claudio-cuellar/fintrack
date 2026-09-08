03452ebaad9238b70e3a341f5ea2ba11b4e12af1
Claudio Cuellar <ccuellar@wine.com>
2026-09-07 22:54:35 -0400

    Fixed prisma service

--- apps/api/src/infrastructure/prisma/prisma.service.ts
+++ apps/api/src/infrastructure/prisma/prisma.service.ts
@@ -5,7 +5,12 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL || '' });
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL || '',
+      ssl: {
+        rejectUnauthorized: false,
+      }
+    });
     super({ adapter });
   }
 
